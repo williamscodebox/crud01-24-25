969bda0fdcbcfc18b88413c5892e628865633193
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-01-25 03:58:01 -0500

    created user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,9 @@
 // Standarized response function
 const handleResponse = (res, status, message, data = null) => {
-
-}
+  res.status(status).json({
+    status,
+    message,
+    data,
+  });
+};
+export const createUser = async (req, res) => {};
